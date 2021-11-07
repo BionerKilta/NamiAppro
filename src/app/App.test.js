897ef3a268a9a85e3678dff1809c24ddfc0879a9
AarniJoensuu <aarni.joensuu.tut@gmail.com>
2021-11-07 14:37:14 +0200

    logo test fixed

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,16 +1,24 @@
-import { render, screen, fireEvent, createEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-// test does not work
-test('test logo rotation', () => {
-  render(<App />);
-  const logoDiv = screen.getByTestId("logo-container");
-  const logo = screen.getByTestId("logo");
-  
-  expect(logo).toHaveClass("spin-counter-clockwise");
-  expect(logo).toHaveClass("animation-paused");
+describe("logo state management should behave correctly", () => {
 
-  fireEvent(logoDiv, new MouseEvent("click"));
+  it("logo should start in non-rotating state" , () => {
+    render(<App />);
+    const logo = screen.getByTestId("logo");
+    expect(logo).toHaveClass("spin-counter-clockwise");
+    expect(logo).toHaveClass("animation-paused");
+  });
 
-  expect(logo).not.toHaveClass("animation-paused");
+  it("logo should rotate after click first and stop rotating after second click", () => {
+    render(<App />);
+    const logoDiv = screen.getByTestId("logo-container");
+    fireEvent.click(logoDiv);
+    const logoAfterFirstClick = screen.getByTestId("logo");
+    expect(logoAfterFirstClick).not.toHaveClass("animation-paused");
+
+    fireEvent.click(logoDiv);
+    const logoAfterSecondClick = screen.getByTestId("logo");
+    expect(logoAfterSecondClick).toHaveClass("animation-paused");
+  });
 });
